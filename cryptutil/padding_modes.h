c492e7fd972106577734a57094e408911d8d62b0
Павел Наумов <da.moscape@gmail.com>
2016-04-30 19:49:13 +0300

    Добавлен режим ANSI X.923

--- cryptutil/padding_modes.h
+++ cryptutil/padding_modes.h
@@ -12,3 +12,11 @@ void pkcs7(byte *block, size_t block_size, size_t filled_blocks) {
 	for (int i = filled_blocks; i < block_size; i++)
 		block[i] =  n;
 }
+
+
+void x923(byte *block, size_t block_size, size_t filled_blocks) {
+	byte n = static_cast<byte>(block_size - filled_blocks);
+	for (int i = filled_blocks; i < block_size - 1; i++)
+		block[i] = 0;
+	block[block_size - 1] = n;
+}
