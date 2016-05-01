ee07ac49c9e61e431d3d247f9e200fbf28f20715
Павел Наумов <da.moscape@gmail.com>
2016-05-01 06:53:52 +0300

    Добавлно шифрование в режиме CFB

--- cryptutil/cipher_modes.h
+++ cryptutil/cipher_modes.h
@@ -6,23 +6,48 @@
 
 using namespace std;
 
-void ecb(istream& ist, ostream& ost, size_t block_size, vector<byte>& key, byte* (*cipher)(byte* block, size_t block_size, vector<byte>& key), void (*padding)(byte *block, size_t block_size, size_t filled_blocks))
+void ecb(istream& ist, ostream& ost, size_t block_size, vector<byte>& key, byte* init_vector, byte* (*cipher)(byte* block, size_t block_size, vector<byte>& key), void (*padding)(byte *block, size_t block_size, size_t filled_blocks))
 {
 	byte *block = new byte[block_size+1];
 	block[block_size] = '\0';
-	byte *new_block;
-	//byte init_vector[] = { 'm', 'i', 'r', '\0' };
+	byte *encrypted;
 
 	while (ist)
 	{
 		ist.read(block, block_size);
 		if (ist.gcount() != block_size)
 			padding(block, block_size, ist.gcount());
-		new_block = cipher(block, block_size, key);
-		ost.write(new_block, block_size);
+		encrypted = cipher(block, block_size, key);
+		ost.write(encrypted, block_size);
 	}
 
 
 	delete[] block;
-	delete[] new_block;
+	delete[] encrypted;
+}
+
+
+void cfb(istream& ist, ostream& ost, size_t block_size, vector<byte>& key, const byte* init_vector, byte* (*cipher)(byte* block, size_t block_size, vector<byte>& key), void(*padding)(byte *block, size_t block_size, size_t filled_blocks))
+{
+	byte *block = new byte[block_size + 1];
+	block[block_size] = '\0';
+	byte *encrypted = new byte[block_size + 1];
+	encrypted[block_size] = '\0';
+
+	memcpy(encrypted, init_vector, block_size+1);
+	encrypted = cipher(block, block_size, key);
+
+	while (ist)
+	{
+		ist.read(block, block_size);
+		if (ist.gcount() != block_size)
+			padding(block, block_size, ist.gcount());
+		for (size_t i = 0; i < block_size; i++)
+			encrypted[i] ^= block[i];
+		ost.write(encrypted, block_size);
+	}
+
+
+	delete[] block;
+	delete[] encrypted;
 }
--- cryptutil/main.cpp
+++ cryptutil/main.cpp
@@ -14,15 +14,20 @@ int main()
 	key.push_back(2);
 	key.push_back(0);
 	key.push_back(1);
+	byte init_vector[] = { 'm', 'i', 'r', '\0' };
 	ifstream ist("in.txt", ios::binary);
 	if (!ist) return -1;
-	//ofstream ost("out.txt", ios::binary);
+	ofstream ost("out.txt", ios::binary);
 	string cmd;
 	do
 	{
 		cin >> cmd;
-		if (cmd == "ecb")
-			ecb(ist, cout, 3, key, transposition, ansi_x923);
+		if (cmd == "cfb")
+			cfb(ist, ost, 3, key, init_vector, transposition, ansi_x923);
 	} while (cmd != "quit");
+	
+	ist.close();
+	ost.close();
+
 	return 0;
 }
