06ba21187514c2aad24b6e44b439d6b898ddf277
Павел Наумов <da.moscape@gmail.com>
2016-05-01 20:35:41 +0300

    Изменены сигнатуры дополнения блоков

--- cryptutil/padding_modes.cpp
+++ cryptutil/padding_modes.cpp
@@ -4,7 +4,7 @@
 
 
 
-void pkcs7(byte *block, size_t block_size, size_t filled_blocks) {
+void pkcs7(byte *block, const size_t block_size, const size_t filled_blocks) {
 	byte n = static_cast<byte>(block_size - filled_blocks);
 	for (size_t i = filled_blocks; i < block_size; i++)
 		block[i] = n;
@@ -13,7 +13,7 @@ void pkcs7(byte *block, size_t block_size, size_t filled_blocks) {
 }
 
 
-void ansi_x923(byte *block, size_t block_size, size_t filled_blocks) {
+void ansi_x923(byte *block, const size_t block_size, const size_t filled_blocks) {
 	byte n = static_cast<byte>(block_size - filled_blocks);
 	for (size_t i = filled_blocks; i < block_size - 1; i++)
 		block[i] = 0;
@@ -23,7 +23,7 @@ void ansi_x923(byte *block, size_t block_size, size_t filled_blocks) {
 }
 
 
-void iso10126(byte *block, size_t block_size, size_t filled_blocks) {
+void iso10126(byte *block, const size_t block_size, const size_t filled_blocks) {
 	byte n = static_cast<byte>(block_size - filled_blocks);
 	for (size_t i = filled_blocks; i < block_size - 1; i++) {
 		srand((unsigned)time(NULL));
--- cryptutil/padding_modes.h
+++ cryptutil/padding_modes.h
@@ -3,14 +3,11 @@
 #include "cryptutil.h"
 
 
-enum PaddingMode
-{ PKCS7, ANSI_X923, ISO_10126 };
+
+void pkcs7(byte *block, const size_t block_size, const size_t filled_blocks);
 
 
-void pkcs7(byte *block, size_t block_size, size_t filled_blocks);
+void ansi_x923(byte *block, const size_t block_size, const size_t filled_blocks);
 
 
-void ansi_x923(byte *block, size_t block_size, size_t filled_blocks);
-
-
-void iso10126(byte *block, size_t block_size, size_t filled_blocks);
+void iso10126(byte *block, const size_t block_size, const size_t filled_blocks);
