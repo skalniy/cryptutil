ccaa8407de68f8150862db67a9125167e6be383e
Павел Наумов <da.moscape@gmail.com>
2016-05-01 18:38:35 +0300

    Добавлен шифр Хилла

--- cryptutil/ciphers.h
+++ cryptutil/ciphers.h
@@ -4,8 +4,7 @@
 
 
 byte* transposition_encrypt(const byte* block, size_t block_size, std::vector<byte>& key) {
-	byte* result;
-	result = static_cast<byte*>(calloc(block_size+1, sizeof(byte)));
+	byte* result = new byte[block_size + 1];
 	result[block_size] = '\0';
 
 	for (size_t i = 0; i < block_size; i++)
@@ -16,12 +15,57 @@ byte* transposition_encrypt(const byte* block, size_t block_size, std::vector<by
 
 
 byte* transposition_decrypt(const byte* block, size_t block_size, std::vector<byte>& key) {
-	byte* result;
-	result = static_cast<byte*>(calloc(block_size + 1, sizeof(byte)));
+	byte* result = new byte[block_size + 1];
 	result[block_size] = '\0';
 
 	for (size_t i = 0; i < block_size; i++)
 		result[i] = block[key[i]];
+
+	return result;
+}
+
+
+byte* vigenere_encrypt(const byte* block, size_t block_size, std::vector<byte>& key) {
+	byte* result = new byte[block_size + 1];
+	result[block_size] = '\0';
+	
+	for (size_t i = 0; i < block_size; i++)
+		result[i] = (block[i] + key[i]) % 256;
+
+	return result;
+}
+
+
+byte* vigenere_decrypt(const byte* block, size_t block_size, std::vector<byte>& key) {
+	byte* result = new byte[block_size + 1];
+	result[block_size] = '\0';
+
+	for (size_t i = 0; i < block_size; i++)
+		result[i] = (block[i] - key[i] + 256) % 256;
+
+	return result;
+}
+
+
+byte* hill_encrypt(const byte* block, size_t block_size, std::vector<byte>& key) {
+	byte* result = new byte[block_size + 1];
+	result[block_size] = '\0';
+	for (size_t i = 0; i < block_size; i++)
+		result[i] = 0;
+
+	for (size_t i = 0; i < block_size; i++)
+		for (size_t j = 0; j < block_size; j++)
+			result[i] += key[i*block_size + j] * block[j] % 257;
+
+	return result;
+}
+
+
+byte* hill_decrypt(const byte* block, size_t block_size, std::vector<byte>& key) {
+	byte* result = new byte[block_size + 1];
+	result[block_size] = '\0';
+
+	
 
 	return result;
 }
--- cryptutil/main.cpp
+++ cryptutil/main.cpp
@@ -5,15 +5,16 @@
 #include "ciphers.h"
 #include "cipher_modes.h"
 
+
 using namespace std;
 
 
 int main()
 {
 	vector<byte> key;
-	key.push_back(2);
+	/*key.push_back(2);
 	key.push_back(0);
-	key.push_back(1);
+	key.push_back(1);*/
 	byte init_vector[] = { 'm', 'i', 'r', '\0' };
 
 	string cmd;
@@ -24,14 +25,19 @@ int main()
 			
 			ifstream ost("out.txt", ios::binary);
 			ofstream d("dec.txt", ios::binary);
-			cbc_decrypt(ost, d, 3, key, init_vector, transposition_decrypt, ansi_x923);
+			ecb_decrypt(ost, d, 3, key, init_vector, vigenere_decrypt, ansi_x923);
 			ost.close();
 			d.close();
 		}
 		else if (cmd == "enc") {
+			int n;
+			for (int i = 0; i < 9; i++) {
+				cin >> n;
+				key.push_back(n);
+			}
 			ifstream ist("in.txt", ios::binary);
 			ofstream ost("out.txt", ios::binary);
-			cbc_encrypt(ist, ost, 3, key, init_vector, transposition_encrypt, ansi_x923);
+			ecb_encrypt(ist, ost, 3, key, init_vector, hill_encrypt, ansi_x923);
 			ost.close();
 			ist.close();
 		}
