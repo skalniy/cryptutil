90ac3fa34c34d030a6e1ddc5e8e92e9c5b466dc1
Павел Наумов <da.moscape@gmail.com>
2016-05-02 09:18:05 +0300

    Созданы классы для режимов шифрования

--- cryptutil/cipher_modes.h
+++ cryptutil/cipher_modes.h
@@ -8,33 +8,34 @@
 using namespace std;
 
 
-template<class Cipher>
 class OperationMode
 {
 public:
 	virtual void encrypt(
 		istream& ist, ostream& ost,
 		const size_t block_size, const vector<byte>& key, const byte *init_vector,
-		void (pad)(byte *block, const size_t block_size, const size_t filled_blocks)
+		byte *(*algorithm)(const byte* block, const size_t block_size, const vector<byte>& key),
+		void (*padding)(byte *block, const size_t block_size, const size_t filled_blocks)
 		) = 0;
 
 
 	virtual void decrypt(
 		istream& ist, ostream& ost,
 		const size_t block_size, const vector<byte>& key, const byte *init_vector,
-		void (pad)(byte *block, const size_t block_size, const size_t filled_blocks)
+		byte *(*algorithm)(const byte* block, const size_t block_size, const vector<byte>& key),
+		void (*padding)(byte *block, const size_t block_size, const size_t filled_blocks)
 		) = 0;
 };
 
 
-template <class Cipher>
-class ECB : OperationMode<Cipher>
+class ECB : OperationMode
 {
 public:
 	static void encrypt(
 		istream& ist, ostream& ost,
 		const size_t block_size, const vector<byte>& key, const byte *init_vector,
-		void (pad)(byte *block, const size_t block_size, const size_t filled_blocks)
+		byte *(*algorithm)(const byte* block, const size_t block_size, const vector<byte>& key),
+		void (*padding)(byte *block, const size_t block_size, const size_t filled_blocks)
 		) 
 	{
 		byte *block = new byte[block_size + 1];
@@ -45,8 +46,8 @@ public:
 			ist.read(block, block_size);
 			if (ist.gcount()) {
 				if (ist.gcount() != static_cast<streamsize>(block_size))
-					pad(block, block_size, static_cast<size_t>(ist.gcount()));
-				byte *encrypted = Cipher::encrypt(block, block_size, key);
+					padding(block, block_size, static_cast<size_t>(ist.gcount()));
+				byte *encrypted = algorithm(block, block_size, key);
 				ost.write(encrypted, block_size);
 				delete[] encrypted;
 			}
@@ -60,7 +61,8 @@ public:
 	static void decrypt(
 		istream& ist, ostream& ost,
 		const size_t block_size, const vector<byte>& key, const byte *init_vector,
-		void (pad)(byte *block, const size_t block_size, const size_t filled_blocks)
+		byte *(*algorithm)(const byte* block, const size_t block_size, const vector<byte>& key),
+		void (*padding)(byte *block, const size_t block_size, const size_t filled_blocks)
 		)
 	{
 		byte *block = new byte[block_size + 1];
@@ -71,8 +73,8 @@ public:
 			ist.read(block, block_size);
 			if (ist.gcount()) {
 				if (ist.gcount() != static_cast<streamsize>(block_size))
-					pad(block, block_size, static_cast<size_t>(ist.gcount()));
-				byte *decrypted = Cipher::decrypt(block, block_size, key);
+					padding(block, block_size, static_cast<size_t>(ist.gcount()));
+				byte *decrypted = algorithm(block, block_size, key);
 				ost.write(decrypted, block_size);
 				delete[] decrypted;
 			}
@@ -84,37 +86,208 @@ public:
 };
 
 
-void cfb_encrypt(istream& ist, ostream& ost, size_t block_size,
-	vector<byte>& key, const byte* init_vector,
-	byte* (*cipher)(const byte* block, const size_t block_size, const vector<byte>& key),
-	void(*padding)(byte *block, const size_t block_size, const size_t filled_blocks));
+class CFB : OperationMode
+{
+public:
+	static void encrypt(
+		istream& ist, ostream& ost,
+		const size_t block_size, const vector<byte>& key, const byte *init_vector,
+		byte *(*algorithm)(const byte* block, const size_t block_size, const vector<byte>& key),
+		void(*padding)(byte *block, const size_t block_size, const size_t filled_blocks)
+		)
+	{
+		byte *block = new byte[block_size + 1];
+		block[block_size] = '\0';
+
+		byte *IV = new byte[block_size + 1];
+		memcpy(IV, init_vector, block_size);
+		IV[block_size] = '\0';
+
+		while (ist) {
+			ist.read(block, static_cast<streamsize>(block_size));
+			if (ist.gcount()) {
+				if (ist.gcount() != static_cast<streamsize>(block_size))
+					padding(block, block_size, static_cast<size_t>(ist.gcount()));
+				byte *encrypted = algorithm(IV, block_size, key);
+				for (size_t i = 0; i < block_size; i++)
+					encrypted[i] ^= block[i];
+				ost.write(encrypted, static_cast<streamsize>(block_size));
+				memmove(IV, encrypted, block_size);
+				delete[] encrypted;
+			}
+		}
+
+		delete[] block;
+		delete[] IV;
+		return;
+	}
+
+	static void decrypt(
+		istream& ist, ostream& ost,
+		const size_t block_size, const vector<byte>& key, const byte *init_vector,
+		byte *(*algorithm)(const byte* block, const size_t block_size, const vector<byte>& key),
+		void(*padding)(byte *block, const size_t block_size, const size_t filled_blocks)
+		)
+	{
+		byte *block = new byte[block_size + 1];
+		block[block_size] = '\0';
+
+		byte *IV = new byte[block_size + 1];
+		memcpy(IV, init_vector, block_size);
+		IV[block_size] = '\0';
+
+		while (ist) {
+			ist.read(block, block_size);
+			if (ist.gcount()) {
+				byte *decrypted = algorithm(IV, block_size, key);
+				for (size_t i = 0; i < block_size; i++)
+					decrypted[i] ^= block[i];
+				ost.write(decrypted, block_size);
+				memmove(IV, block, block_size);
+				delete[] decrypted;
+			}
+		}
+
+		delete[] block;
+		delete[] IV;
+		return;
+	}
+};
 
 
-void cfb_decrypt(istream& ist, ostream& ost, size_t block_size,
-	vector<byte>& key, const byte* init_vector,
-	byte* (*cipher)(const byte* block, const size_t block_size, const vector<byte>& key),
-	void(*padding)(byte *block, const size_t block_size, const size_t filled_blocks));
+class OFB : OperationMode
+{
+public:
+	static void encrypt(
+		istream& ist, ostream& ost,
+		const size_t block_size, const vector<byte>& key, const byte *init_vector,
+		byte *(*algorithm)(const byte* block, const size_t block_size, const vector<byte>& key),
+		void(*padding)(byte *block, const size_t block_size, const size_t filled_blocks)
+		)
+	{
+		byte *block = new byte[block_size + 1];
+		block[block_size] = '\0';
+
+		byte *IV = new byte[block_size + 1];
+		memcpy(IV, init_vector, block_size);
+		IV[block_size] = '\0';
+
+		while (ist) {
+			ist.read(block, static_cast<streamsize>(block_size));
+			if (ist.gcount()) {
+				if (ist.gcount() != static_cast<streamsize>(block_size))
+					padding(block, block_size, static_cast<size_t>(ist.gcount()));
+				byte *encrypted = algorithm(IV, block_size, key);
+				memcpy(IV, encrypted, block_size);
+				for (size_t i = 0; i < block_size; i++)
+					encrypted[i] ^= block[i];
+				ost.write(encrypted, static_cast<streamsize>(block_size));
+				delete[] encrypted;
+			}
+		}
+
+		delete[] block;
+		delete[] IV;
+		return;
+	}
+
+	static void decrypt(
+		istream& ist, ostream& ost,
+		const size_t block_size, const vector<byte>& key, const byte *init_vector,
+		byte *(*algorithm)(const byte* block, const size_t block_size, const vector<byte>& key),
+		void(*padding)(byte *block, const size_t block_size, const size_t filled_blocks)
+		)
+	{
+		byte *block = new byte[block_size + 1];
+		block[block_size] = '\0';
+
+		byte *IV = new byte[block_size + 1];
+		memcpy(IV, init_vector, block_size);
+		IV[block_size] = '\0';
+
+		while (ist) {
+			ist.read(block, block_size);
+			if (ist.gcount()) {
+				byte *decrypted = algorithm(IV, block_size, key);
+				memcpy(IV, decrypted, block_size);
+				for (size_t i = 0; i < block_size; i++)
+					decrypted[i] ^= block[i];
+				ost.write(decrypted, block_size);
+				delete[] decrypted;
+			}
+		}
+
+		delete[] block;
+		delete[] IV;
+		return;
+	}
+};
 
 
-void ofb_encrypt(istream& ist, ostream& ost, size_t block_size,
-	vector<byte>& key, byte* init_vector,
-	byte* (*cipher)(const byte* block, const size_t block_size, const vector<byte>& key),
-	void(*padding)(byte *block, const size_t block_size, const size_t filled_blocks));
+class CBC : OperationMode
+{
+public:
+	static void encrypt(
+		istream& ist, ostream& ost,
+		const size_t block_size, const vector<byte>& key, const byte *init_vector,
+		byte *(*algorithm)(const byte* block, const size_t block_size, const vector<byte>& key),
+		void(*padding)(byte *block, const size_t block_size, const size_t filled_blocks)
+		)
+	{
+		byte *block = new byte[block_size + 1];
+		block[block_size] = '\0';
 
+		byte *IV = new byte[block_size + 1];
+		memcpy(IV, init_vector, block_size);
+		IV[block_size] = '\0';
 
-void ofb_decrypt(istream& ist, ostream& ost, size_t block_size,
-	vector<byte>& key, const byte* init_vector,
-	byte* (*cipher)(const byte* block, const size_t block_size, const vector<byte>& key),
-	void(*padding)(byte *block, const size_t block_size, const size_t filled_blocks));
+		while (ist) {
+			ist.read(block, static_cast<streamsize>(block_size));
+			if (ist.gcount()) {
+				if (ist.gcount() != static_cast<streamsize>(block_size))
+					padding(block, block_size, static_cast<size_t>(ist.gcount()));
+				for (size_t i = 0; i < block_size; i++)
+					IV[i] ^= block[i];
+				byte *encrypted = algorithm(IV, block_size, key);
+				memcpy(IV, encrypted, block_size);
+				ost.write(encrypted, static_cast<streamsize>(block_size));
+				delete[] encrypted;
+			}
+		}
 
+		delete[] block;
+		delete[] IV;
+		return;
+	}
 
-void cbc_encrypt(istream& ist, ostream& ost, size_t block_size,
-	vector<byte>& key, byte* init_vector,
-	byte* (*cipher)(const byte* block, const size_t block_size, const vector<byte>& key),
-	void(*padding)(byte *block, const size_t block_size, const size_t filled_blocks));
+	static void decrypt(
+		istream& ist, ostream& ost,
+		const size_t block_size, const vector<byte>& key, const byte *init_vector,
+		byte *(*algorithm)(const byte* block, const size_t block_size, const vector<byte>& key),
+		void(*padding)(byte *block, const size_t block_size, const size_t filled_blocks)
+		)
+	{
+		byte *block = new byte[block_size + 1];
+		block[block_size] = '\0';
 
+		byte *IV = new byte[block_size + 1];
+		memcpy(IV, init_vector, block_size);
+		IV[block_size] = '\0';
 
-void cbc_decrypt(istream& ist, ostream& ost, size_t block_size,
-	vector<byte>& key, const byte* init_vector,
-	byte* (*cipher)(const byte* block, const size_t block_size, const vector<byte>& key),
-	void(*padding)(byte *block, const size_t block_size, const size_t filled_blocks));
+		while (ist) {
+			ist.read(block, block_size);
+			if (ist.gcount()) {
+				byte *decrypted = algorithm(block, block_size, key);
+				for (size_t i = 0; i < block_size; i++)
+					decrypted[i] ^= IV[i];
+				memcpy(IV, block, block_size);
+				ost.write(decrypted, block_size);
+				delete[] decrypted;
+			}
+		}
+
+		delete[] block;
+		delete[] IV;
+		return;
+	}
+};
--- cryptutil/main.cpp
+++ cryptutil/main.cpp
@@ -26,7 +26,7 @@ int main()
 			
 			ifstream ost("out.txt", ios::binary);
 			ofstream d("dec.txt", ios::binary);
-			ECB<Vigenere>::decrypt(ost, d, 3, key, init_vector, Padding::ansi_x923);
+			OFB::decrypt(ost, d, 3, key, init_vector, Vigenere::encrypt, Padding::iso10126);
 			ost.close();
 			d.close();
 		}
@@ -38,7 +38,7 @@ int main()
 			}*/
 			ifstream ist("in.txt", ios::binary);
 			ofstream ost("out.txt", ios::binary);
-			ECB<Vigenere>::encrypt(ist, ost, 3, key, init_vector, Padding::iso10126);
+			OFB::encrypt(ist, ost, 3, key, init_vector, Vigenere::encrypt, Padding::iso10126);
 			ost.close();
 			ist.close();
 		}
