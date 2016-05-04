8641e1d4be5b8f2829d47c516f9d21de0e4f92d0
Павел Наумов <da.moscape@gmail.com>
2016-05-05 00:02:38 +0300

    Рефакторинг

--- cryptutil/cryptutil.h
+++ cryptutil/cryptutil.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <vector>
 #include <list>
-#include <sstream>
 
 
 
@@ -10,6 +9,6 @@ using namespace std;
 
 typedef char byte;
 typedef vector<byte> TKey;
-typedef byte* (*crypto_algorithm)(const byte*, const size_t, const TKey&);
-typedef void(*padding_algorithm)(byte *, const size_t, const size_t);
+typedef byte* (*crypto_algorithm)(const byte *, const size_t, const TKey&);
+typedef void (*padding_algorithm)(byte *, const size_t, const size_t);
 typedef list<string> TChain;
